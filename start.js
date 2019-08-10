db9fb3e00a58f1fc131b1fc5ee05753a8cda90c2
Damián Alvarez <alvarezdami93@gmail.com>
2019-08-10 01:22:23 -0300

    Add @babel/polyfill dependency

--- start.js
+++ start.js
@@ -1,3 +1,4 @@
+require("@babel/polyfill");
 // Transpile all code following this line with babel and use '@babel/preset-env' (aka ES6) preset.
 require("@babel/register")({
     presets: ["@babel/preset-env"]
